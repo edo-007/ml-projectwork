34712808c7217e148700079cd3f541727a66381b
edo-007 <edofau07@gmail.com>
2025-11-03 18:23:58 +0100

    stats

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 MODELS=('logreg' 'svm' 'dt' 'knn')
 TRAIN_SIZES=(0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.00)
-# python learning_curve.py --data data/new_Covid.csv --train-sizes "${TRAIN_SIZES[@]}" --models "${MODELS[@]}"
+python -m src.learning_curve --data new_Covid.csv --seedsplit 25 --train-sizes "${TRAIN_SIZES[@]}" --models "${MODELS[@]}"
